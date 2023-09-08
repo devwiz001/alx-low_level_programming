db078f6299683010b9048689115cb08de40013ba
devwiz001 <wisdomokpara04@gmail.com>
2023-09-08 11:51:02 -0700

    finally fixed typos

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 /**
- * main - this is a program that prints the size of various types
- * Return: 0(sucess)
+ * main - this is the  main function
+ * Return:0
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+        printf("Size of a char: %lu byte(s)\n", sizeof(char));
+        printf("Size of an int: %lu byte(s)\n", sizeof(int));
+        printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+        printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+        printf("Size of a float: %lu byte(s)\n", sizeof(float));
+        return (0);
 }
