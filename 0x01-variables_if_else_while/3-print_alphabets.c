65c42ffe9050f8b06d458f8bfdd5f4a3936da496
devwiz001 <wisdomokpara04@gmail.com>
2023-09-08 20:06:41 -0700

    third task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - void
+ * Return: 0 (always successful)
+ */
+
+int main(void)
+{
+	char a;
+
+	char b;
+
+	a = 'c';
+	b = 'C';
+	while
+		(a <= 'd') {
+			putchar(a);
+			a++;
+		}
+	while
+		(b <= 'D') {
+			putchar (b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
