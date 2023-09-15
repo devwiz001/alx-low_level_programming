5cf88fba21bdecd7fe61403b7cf69eee93b9e2d6
devwiz001 <wisdomokpara04@gmail.com>
2023-09-14 19:58:43 -0700

    ...

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line of backslashes
+ * @n: the number of times the character '\' should be printed
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < 1; j++)
+			{
+				_putchar(32);
+			}
+			_putchar('\\');
+			_putchar(10);
+		}
+	}
+}
