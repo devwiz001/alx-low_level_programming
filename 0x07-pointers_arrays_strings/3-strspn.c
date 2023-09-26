10a2e53eb3c65abf0cc8463302a840559c6fcb39
devwiz001 <wisdomokpara04@gmail.com>
2023-09-26 15:34:40 -0700

    length of a prefux substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length
+ * @s: A pointer
+ * @accept: A pointer
+ *
+ * Return: The number of byte
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i, j;
+	int found;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		found = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				found = 1;
+				break;
+			}
+		}
+
+		if (found == 0)
+		{
+			return (count);
+		}
+	}
+
+	return (count);
+}
+
