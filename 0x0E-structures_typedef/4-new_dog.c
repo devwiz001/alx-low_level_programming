8e705bd4acf45ee919f5fbb292b10ff9c8f475c1
devwiz001 <wisdomokpara04@gmail.com>
2023-10-11 18:59:05 -0700

    pushing everything

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - this creates a new dog structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: pointer to structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	d->name = malloc(strlen(name) + 1);
+	d->owner = malloc(strlen(owner) + 1);
+	if (d->name == NULL || d->owner == NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (NULL);
+	}
+	strcpy(d->name,  name);
+	d->age = age;
+	strcpy(d->owner, owner);
+	return (d);
+}
