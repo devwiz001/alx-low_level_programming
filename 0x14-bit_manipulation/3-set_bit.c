28b4f1fade915dbe3f0f1f1255bb5f2b7445669b
devwiz001 <wisdomokpara04@gmail.com>
2023-10-26 18:28:00 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8)) /* Check if index is out of range */
+	if (index >= (sizeof(unsigned long int) * 
 		return (-1);
 
-	*n = *n | (1UL << index); /* Set the bit at the specified index to 1 */
+	*n = *n | (1UL << index);
 
 	return (1);
 }
