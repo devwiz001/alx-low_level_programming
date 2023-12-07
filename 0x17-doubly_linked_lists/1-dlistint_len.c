87f81e363243e4ed0459d72c50e5e44bb624b854
devwiz001 <wisdomokpara04@gmail.com>
2023-12-07 14:37:55 -0800

    dlistint

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,10 @@
 #include "lists.h"
-
 /**
- *  * dlistint_len - returns the number of elements in
- *   * a double linked list
- *    *
- *     * @h: head of the list
- *      * Return: the number of nodes
- *       */
+ * dlistint_len - tgis always returns the number of elements in
+ * a double linked list
+ * @h: this the head of the list
+ * Return: retirns the number of nodes
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
